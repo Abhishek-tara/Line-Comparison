338642b1e488d07812b5216ca9e2781b76b94bee
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-04-24 08:49:41 +0530

    Added Line Comparison

---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LineComparison
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Camparison Problem");
+            CalculateLine firstLine = new CalculateLine(2, 3, 4, 5);
+            firstLine.Calculate();
+        }
+    }
+}
