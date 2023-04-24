aa1563a3672df55062f7d1820ff1bb6a960e54be
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-04-24 09:42:03 +0530

    Using compare method to compare two lines

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -8,13 +8,20 @@
             CalculateLine obj = new CalculateLine(2, 3, 4, 5);
             double length_One =  obj.Calculate();
 
-            CalculateLine obj_Two = new CalculateLine(4, 8, 6, 9);
+            CalculateLine obj_Two = new CalculateLine(2, 3, 4, 5);
             double length_Two = obj_Two.Calculate();
 
             if (length_One.Equals(length_Two))           
                 Console.WriteLine("Both lines are equal");         
             else
                 Console.WriteLine("Not equal");
+
+            if (length_One.CompareTo(length_Two) == 0)
+                Console.WriteLine("Both lines are equal");
+            else if (length_One.CompareTo(length_Two) > 0)
+                Console.WriteLine("Line one is greater than line two");
+            else
+                Console.WriteLine("Line Two is greater than line one");
         }
     }
 }
